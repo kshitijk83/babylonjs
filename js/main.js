a7a0937ff23e0744b184756ccdfbb34845e62fca
kshitij kumar <kshitijk83@gmail.com>
2018-10-18 13:06:47 +0530

    bounding boxes around dude

--- js/main.js
+++ js/main.js
@@ -10,8 +10,10 @@ var isDPressed = false;
 document.addEventListener("DOMContentLoaded", startGame);
 
 class Dude {
-    constructor(dudeMesh,speed) {
+    constructor(dudeMesh,speed, id, scene, scaling) {
         this.dudeMesh=  dudeMesh;
+        this.id = id;
+        this.scene = scene;
         dudeMesh.Dude = this;
         if(speed) {
             this.speed = speed;
@@ -19,9 +21,25 @@ class Dude {
         else {
             this.speed = 1;
         }
+        if(scaling) {
+            this.scaling = scaling;
+            this.dudeMesh.scaling = new BABYLON.Vector3(this.scaling, this.scaling, this.scaling);
+        }
+        else
+            this.scaling = 1;
+
+        if(Dude.boundingBoxParameters==undefined) {
+            Dude.boundingBoxParameters = this.CalculateBoundingBoxParameters(); // this will create bounding box parameters for first dude and for other dudes it already has these parameters so it will skip it
+        }
+
+        this.bounder = this.createBoundingBox();
+        this.bounder.dudeMesh = this.dudeMesh;
     }
 
     move() {
+
+        if(!this.bounder) return;
+        this.dudeMesh.position = new BABYLON.Vector3(this.bounder.position.x, this.bounder.position.y-this.scaling*Dude.boundingBoxParameters.lengthY/2, this.bounder.position.z);
         var tank = scene.getMeshByName("heroTank");
         var direction = tank.position.subtract(this.dudeMesh.position);
         var distance = direction.length();
@@ -29,10 +47,79 @@ class Dude {
         var alpha = Math.atan2(-1*dir.x, -1*dir.z);
         this.dudeMesh.rotation.y = alpha;
         if(distance>30) {
-            this.dudeMesh.moveWithCollisions(dir.multiplyByFloats(this.speed, this.speed, this.speed)); // if distance between dude and tank is greater 30, move the dude towards the tank
+            this.bounder.moveWithCollisions(dir.multiplyByFloats(this.speed, this.speed, this.speed)); // if distance between dude and tank is greater 30, move the dude towards the tank
         }
+    }
+
+    createBoundingBox() {
+        var lengthX = Dude.boundingBoxParameters.lengthX;
+        var lengthY = Dude.boundingBoxParameters.lengthY;
+        var lengthZ = Dude.boundingBoxParameters.lengthZ;
+
+        var bounder = new BABYLON.Mesh.CreateBox("bounder" + (this.id).toString(),  1, this.scene); // parameters=> id, size or scale, scene
+
+        bounder.scaling.x = lengthX*this.scaling; // we have to multiply it by a scaling no. bcoz lengthX is in modeling space
+        bounder.scaling.y = lengthY*this.scaling;
+        bounder.scaling.z = lengthZ*this.scaling*2;
+
+        bounder.isVisible = false;
+
+        var bounderMaterial = new BABYLON.StandardMaterial("bounderMaterial", this.scene);
+        bounderMaterial.alpha = .5;
+        bounder.material = bounderMaterial;
+        bounder.checkCollisions = true;
+
+        bounder.position = new BABYLON.Vector3(this.dudeMesh.position.x, this.dudeMesh.position.y+this.scaling*lengthY/2, this.dudeMesh.position.z); // babylon js will create a bounder position at origin of the scene initial that is why all dudes are in the same place and colliding and also we will correct the location of the bounderbox
+        return bounder;
+    }
+
+    CalculateBoundingBoxParameters() {
+        var minX=999999; var minY=99999; var minZ=999999;
+        var maxX=-99999; var maxY=-999999; var maxZ=-99999;
+
+        var children = this.dudeMesh.getChildren();
+
+        for (var i = 0 ; i < children.length ; i++) {
+            var positions = new BABYLON.VertexData.ExtractFromGeometry(children[i]).positions;
+            if (!positions) continue;
+
+            var index = 0;
+            for (var j = index ; j < positions.length ; j += 3) {
+                if (positions[j] < minX)
+                    minX = positions[j];
+                if (positions[j] > maxX)
+                    maxX = positions[j];
+            }
+            index = 1;
+            for (var j = index ; j < positions.length ; j += 3) {
+                if (positions[j] < minY)
+                    minY = positions[j];
+                if (positions[j] > maxY)
+                    maxY = positions[j];
+            }
+            index = 2;
+            for (var j = index ; j < positions.length ; j += 3) {
+                if (positions[j] < minZ)
+                    minZ = positions[j];
+                if (positions[j] > maxZ)
+                    maxZ = positions[j];
+            }
+
+            var _lengthX = maxX - minX;
+            var _lengthY = maxY - minY;
+            var _lengthZ = maxZ - minZ;
+            
+
+        }
+        
+        console.log(_lengthX);
+            console.log(_lengthY);
+            console.log(_lengthZ);
+
+        return {lengthX: _lengthX, lengthY: _lengthY, lengthZ: _lengthZ};
+    }
 }
-}
+
 
 function startGame() {
     canvas = document.getElementById("renderCanvas");
@@ -229,17 +316,15 @@ function createHeroDude(scene) {
         newMeshes[0].position = new BABYLON.Vector3(0, 0, 5);  // The original dude
         newMeshes[0].name = "heroDude"; // changing the name of dude
         var heroDude = newMeshes[0];
-        heroDude.scaling = new BABYLON.Vector3(.2,.2,.2);
-        heroDude.speed = 2;
         scene.beginAnimation(skeletons[0], 0, 120, true, 1.0); // animation of walking ==parameters=> skeleton part ou want to render animation, starting of keyframe, ending of keyframes, loopback, playbackspeed
         
-        var hero = new Dude(heroDude,2);
+        var hero = new Dude(heroDude, 2, -1, scene, .2);
 
         scene.dudes = [];
         for(var q=0; q<10; q++) {
             scene.dudes[q] = DoClone(heroDude, skeletons, q);
             scene.beginAnimation(scene.dudes[q].skeleton, 0, 120, true, 1.0); // animating the dudes
-            var temp = new Dude(scene.dudes[q], 2);
+            var temp = new Dude(scene.dudes[q], 2, q, scene, .2);
         }
         }
     }
@@ -270,8 +355,9 @@ function DoClone(original, skeletons, id) { // making dude clones
             } else if(skeleton.length==original.getChildren().length) { // all children has its own skeleton
                 for(var i=0; i<myClone.getChildren().length; i++){
                     myClone.getChildren()[i].skeleton = skeletons[i].clone("clone_"+id+"_skeleton_"+i);
-                    return myClone;
+                    
                 }
+                return myClone;
             }
         }
     }
